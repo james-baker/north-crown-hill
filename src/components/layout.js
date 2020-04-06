5634f4dc1337ef65aa6b99e0df015b1fbfdb9e0b
James Baker <james-baker@users.noreply.github.com>
2020-04-06 16:41:11 -0700

    import the grids css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../styles/pure-min.css"
+import "../styles/grids-responsive-min.css"
 import Header from "./header"
 import "./layout.css"
 export default ({ children }) => <div><Header></Header>{children}</div>
