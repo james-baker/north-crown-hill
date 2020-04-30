bbe53ba3cc47474f450683e349479b08d84a44ad
James Baker <james-baker@users.noreply.github.com>
2020-04-30 14:33:19 -0700

    bring newsletter archive to this site, including latest

--- src/components/mailchimparchive.js
+++ src/components/mailchimparchive.js
@@ -1,14 +1,20 @@
 import React from "react"
 import "./mailchimparchive.css"
 
+const ArchiveLink = props => (
+  <li class="campaign">{props.date} - <a href={props.to} title={props.children}>{props.children}</a>
+  </li>
+)
+
 export default () => <div>
   <p>Some recent newsletters:</p>
   <ul id="archive-list">
     <div class="display_archive">
-      <li class="campaign">04/08/2020 - <a href="http://us19.campaign-archive.com/?u=22899abf63f333cc474ce3a93&id=0a84af55e7" title="North Crown Hill News, April 8th" target="_blank" rel="noopener noreferrer">North Crown Hill News, April 8th</a></li>
-      <li class="campaign">03/31/2020 - <a href="http://us19.campaign-archive.com/?u=22899abf63f333cc474ce3a93&id=6bb61a9654" title="North Crown Hill News, March 30th" target="_blank" rel="noopener noreferrer">North Crown Hill News, March 30th</a></li>
-      <li class="campaign">03/22/2020 - <a href="http://us19.campaign-archive.com/?u=22899abf63f333cc474ce3a93&id=1b44a196fb" title="North Crown Hill News, March 21st" target="_blank" rel="noopener noreferrer">North Crown Hill News, March 21st</a></li>
-      <li class="campaign">03/16/2020 - <a href="http://us19.campaign-archive.com/?u=22899abf63f333cc474ce3a93&id=56c7b00d94" title="North Crown Hill launches!" target="_blank" rel="noopener noreferrer">North Crown Hill launches!</a></li>
+      <ArchiveLink to="/newsletter-archive/20200428.html" date="04/28/2020">North Crown Hill: Community Hotline!</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200408.html" date="04/08/2020">North Crown Hill News, April 8th"</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200330.html" date="03/30/2020">North Crown Hill News, March 30th"</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200321.html" date="03/21/2020">North Crown Hill News, March 21st"</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200316.html" date="03/16/2020">"North Crown Hill launches!"</ArchiveLink>
     </div>
   </ul>
 </div>
