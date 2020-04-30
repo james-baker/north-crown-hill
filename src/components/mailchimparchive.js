871d446aebba723f2c535b959dcf60e5635ba577
James Baker <james-baker@users.noreply.github.com>
2020-04-30 15:06:34 -0700

    remove leftover quotes

--- src/components/mailchimparchive.js
+++ src/components/mailchimparchive.js
@@ -11,10 +11,10 @@ export default () => <div>
   <ul id="archive-list">
     <div class="display_archive">
       <ArchiveLink to="/newsletter-archive/20200428.html" date="04/28/2020">North Crown Hill: Community Hotline!</ArchiveLink>
-      <ArchiveLink to="/newsletter-archive/20200408.html" date="04/08/2020">North Crown Hill News, April 8th"</ArchiveLink>
-      <ArchiveLink to="/newsletter-archive/20200330.html" date="03/30/2020">North Crown Hill News, March 30th"</ArchiveLink>
-      <ArchiveLink to="/newsletter-archive/20200321.html" date="03/21/2020">North Crown Hill News, March 21st"</ArchiveLink>
-      <ArchiveLink to="/newsletter-archive/20200316.html" date="03/16/2020">"North Crown Hill launches!"</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200408.html" date="04/08/2020">North Crown Hill News, April 8th</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200330.html" date="03/30/2020">North Crown Hill News, March 30th</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200321.html" date="03/21/2020">North Crown Hill News, March 21st</ArchiveLink>
+      <ArchiveLink to="/newsletter-archive/20200316.html" date="03/16/2020">North Crown Hill launches!</ArchiveLink>
     </div>
   </ul>
 </div>
